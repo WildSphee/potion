eb83bf69bb403aa29569a924059db965f482e0e1
Reagan <rrr.chanhiulok@gmail.com>
2024-10-16 05:30:53 +0000

    update potion

--- potion.py
+++ potion.py
@@ -7,9 +7,7 @@ from pydantic import BaseModel, Field, TypeAdapter, ValidationError
 
 from llms.openai import call_openai
 from llms.ppt_prompt import ppt_prompt
-from tools.validator import trim_code_block
-from typing import Callable, Optional
-import asyncio
+from tools.validator import convert_to_filename, trim_code_block
 
 """Logic Flow
 
@@ -53,6 +51,9 @@ class ComposeSchema(BaseModel):
     func: Callable = Field(
         ...,
         description="Function to input the schema and create the slide, returns a pptx slide obj",
+    )
+    slide_layout_index: int = Field(
+        ..., description="which slide layout to use for each slide type"
     )
 
 
@@ -106,17 +107,39 @@ class Potion:
             )
             return {"error": error_type, "details": str(e)}
 
-    async def compose(self, design_schema: List[DesignSchema]) -> os.PathLike:
-        """Create the PPTX base on requirement"""
+    async def compose(self, design_schemas: List[DesignSchema]) -> None:
+        """Create the PPTX based on the design schema list"""
 
-        tasks: List = []
+        for ds in design_schemas:
+            # Find the corresponding ComposeSchema
+            print("COMPOSING", ds)
+            compose_schema = next(
+                (
+                    cs
+                    for cs in self.compose_schemas
+                    if cs.name == ds.compose_schema_name
+                ),
+                None,
+            )
+            if compose_schema is None:
+                continue
 
-        for ds in design_schema:
-            # get the corresponding ComposeSchema
-            cs: Optional[ComposeSchema] = next((cs for cs in self.compose_schemas if cs.name == ds.compose_schema_name), None)
-            func: Callable = cs.func
-            tasks.append(asyncio.create_task(func(ds)))
+            # Get the slide layout to use
+            slide_layout = self.presentation.slide_layouts[
+                compose_schema.slide_layout_index
+            ]
 
+            # Add a new slide based on the layout
+            slide = self.presentation.slides.add_slide(slide_layout)
 
-        results: List = await asyncio.gather(*tasks)
-        
+            # Call the compose function to modify the slide
+            await compose_schema.func(ds, slide)
+
+    def save(self) -> os.PathLike:
+        filename = convert_to_filename("output")
+        output_path: os.PathLike = (
+            f'{os.getenv("PPT_OUTPUT_FOLDER") or "output/"}{filename}.pptx'
+        )
+        self.presentation.save(output_path)
+
+        return output_path
